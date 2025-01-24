05c641f74d31effda82e2b9e338764d03bf8be82
oppqrz <jsamuelcolburn@gmail.com>
2025-01-24 23:16:55 +0000

    updated formating and cmake lists

--- easyBuildScript.sh
+++ easyBuildScript.sh
@@ -1,2 +1,2 @@
-cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=/home/oppqrz/vcpkg/scripts/buildsystems/vcpkg.cmake~~
+cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=/home/oppqrz/vcpkg/scripts/buildsystems/vcpkg.cmake~~ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON 
 cmake --build build
