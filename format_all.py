bca35df7812a1cb9588afe91d511fa572a56497a
oppqrz <jsamuelcolburn@gmail.com>
2025-01-24 22:33:20 +0000

    Added clang-format pythonscript for simplicity of lintting

---
+++ format_all.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+# Specify file extensions to format
+extensions = (".cpp", ".hpp", ".c", ".h")
+
+# Walk through the directory
+for root, _, files in os.walk("./data"):
+    for file in files:
+        if file.endswith(extensions):
+            filepath = os.path.join(root, file)
+            subprocess.run(["clang-format", "--style=Google", filepath])
+
+# Walk through the directory
+for root, _, files in os.walk("./include"):
+    for file in files:
+        if file.endswith(extensions):
+            filepath = os.path.join(root, file)
+            subprocess.run(["clang-format", "--style=Google", filepath])
+
+
+# Walk through the directory
+for root, _, files in os.walk("./tests"):
+    for file in files:
+        if file.endswith(extensions):
+            filepath = os.path.join(root, file)
+            subprocess.run(["clang-format", "--style=Google", filepath])
