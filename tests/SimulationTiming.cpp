da0db54f53bb102732da88b96b0cb90d7d722678
oppqrz <jsamuelcolburn@gmail.com>
2025-01-27 16:35:04 +0000

    Added simuation timings tests to allow the development of simulation optimsation tests

---
+++ tests/SimulationTiming.cpp
@@ -0,0 +1,51 @@
+#include <chrono>
+#include <iostream>
+
+#include "gtest/gtest.h"
+#include "../include/core/SimulateTime.hpp"
+
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
+
+
+TEST(SimulationTiming, InnerSolarTimings) {
+    // Initialize a copy of the solar system
+    std::vector<Body> bodies = InnerSolarSystem::AllBodies;
+
+    auto t1 = high_resolution_clock::now();
+    // Simulate the system for 1 year
+    bodies = SimulateTimeAndRender(bodies,365.25  * 24 * 60 * 60);
+    
+    auto t2 = high_resolution_clock::now();
+    
+    /* Getting number of milliseconds as an integer. */
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+    /* Getting number of milliseconds as a double. */
+    duration<double, std::milli> ms_double = t2 - t1;
+
+    std::cout << ms_int.count() << "ms\n";
+    std::cout << ms_double.count() << "ms\n";
+}
+
+TEST(SimulationTiming, FullSolarTimings) {
+    // Initialize a copy of the solar system
+    std::vector<Body> bodies = SolarSystem::AllBodies;
+
+    auto t1 = high_resolution_clock::now();
+    // Simulate the system for 1 year
+    bodies = SimulateTimeAndRender(bodies,365.25  * 24 * 60 * 60);
+    
+    auto t2 = high_resolution_clock::now();
+    
+    /* Getting number of milliseconds as an integer. */
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+    /* Getting number of milliseconds as a double. */
+    duration<double, std::milli> ms_double = t2 - t1;
+
+    std::cout << ms_int.count() << "ms\n";
+    std::cout << ms_double.count() << "ms\n";
+}
